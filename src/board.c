7737ecfb764d37424cea5ac7eec6f8f6759642c2
svitharana <svitharana203@gmail.com>
2026-08-03 11:07:07 +0530

    Updated board values

--- src/board.c
+++ src/board.c
@@ -26,162 +26,242 @@ void initialize_board(Square *board)
     board[0].square_name = "GO";
     board[0].square_type = SPECIAL;
     board[0].property_group = NONE;
+    board[0].purchase_price = 0;
+    board[0].base_rent = 0;
 
     board[1].square_name = "Pettah";
     board[1].square_type = PROPERTY;
     board[1].property_group = BROWN;
+    board[1].purchase_price = 1500;
+    board[1].base_rent = 100;
 
     board[2].square_name = "Community Development Fund";
     board[2].square_type = EVENT;
     board[2].property_group = NONE;
+    board[2].purchase_price = 0;
+    board[2].base_rent = 0;
 
     board[3].square_name = "Maradana";
     board[3].square_type = PROPERTY;
     board[3].property_group = BROWN;
+    board[3].purchase_price = 1800;
+    board[3].base_rent = 120;
 
     board[4].square_name = "Income Tax";
     board[4].square_type = TAX;
     board[4].property_group = NONE;
+    board[4].purchase_price = 0;
+    board[4].base_rent = 0;
 
     board[5].square_name = "Colombo Fort Railway Station";
     board[5].square_type = RAILWAY;
     board[5].property_group = NONE;
+    board[5].purchase_price = 0;
+    board[5].base_rent = 0;
 
     board[6].square_name = "Bambalapitiya";
     board[6].square_type = PROPERTY;
     board[6].property_group = LIGHT_BLUE;
+    board[6].purchase_price = 2500;
+    board[6].base_rent = 180;
 
     board[7].square_name = "National Event Card";
     board[7].square_type = EVENT;
     board[7].property_group = NONE;
+    board[7].purchase_price = 0;
+    board[7].base_rent = 0;
 
     board[8].square_name = "Wellawatte";
     board[8].square_type = PROPERTY;
     board[8].property_group = LIGHT_BLUE;
+    board[8].purchase_price = 2700;
+    board[8].base_rent = 200;
 
     board[9].square_name = "Mount Lavinia";
     board[9].square_type = PROPERTY;
     board[9].property_group = LIGHT_BLUE;
+    board[9].purchase_price = 3000;
+    board[9].base_rent = 220;
 
     board[10].square_name = "Jail / Just Visiting";
     board[10].square_type = JAIL;
     board[10].property_group = NONE;
+    board[10].purchase_price = 0;
+    board[10].base_rent = 0;
 
     board[11].square_name = "Nugegoda";
     board[11].square_type = PROPERTY;
     board[11].property_group = PINK;
+    board[11].purchase_price = 3500;
+    board[11].base_rent = 260;
 
     board[12].square_name = "Ceylon Electricity Board";
     board[12].square_type = UTILITY;
     board[12].property_group = NONE;
+    board[12].purchase_price = 0;
+    board[12].base_rent = 0;
 
     board[13].square_name = "Maharagama";
     board[13].square_type = PROPERTY;
     board[13].property_group = PINK;
+    board[13].purchase_price = 3800;
+    board[13].base_rent = 280;
 
     board[14].square_name = "Kottawa";
     board[14].square_type = PROPERTY;
     board[14].property_group = PINK;
+    board[14].purchase_price = 4000;
+    board[14].base_rent = 300;
 
     board[15].square_name = "Kandy Railway Station";
     board[15].square_type = RAILWAY;
     board[15].property_group = NONE;
+    board[15].purchase_price = 0;
+    board[15].base_rent = 0;
 
     board[16].square_name = "Negombo";
     board[16].square_type = PROPERTY;
     board[16].property_group = ORANGE;
+    board[16].purchase_price = 4500;
+    board[16].base_rent = 350;
 
     board[17].square_name = "Sri Lanka Insurance";
     board[17].square_type = INSURANCE;
     board[17].property_group = NONE;
+    board[17].purchase_price = 0;
+    board[17].base_rent = 0;
 
     board[18].square_name = "Katunayake";
     board[18].square_type = PROPERTY;
     board[18].property_group = ORANGE;
+    board[18].purchase_price = 4700;
+    board[18].base_rent = 370;
 
     board[19].square_name = "Ja-Ela";
     board[19].square_type = PROPERTY;
     board[19].property_group = ORANGE;
+    board[19].purchase_price = 5000;
+    board[19].base_rent = 400;
 
     board[20].square_name = "Free Parking";
     board[20].square_type = SPECIAL;
     board[20].property_group = NONE;
+    board[20].purchase_price = 0;
+    board[20].base_rent = 0;
 
     board[21].square_name = "Kandy City";
     board[21].square_type = PROPERTY;
     board[21].property_group = RED;
+    board[21].purchase_price = 5500;
+    board[21].base_rent = 450;
 
     board[22].square_name = "National Event Card";
     board[22].square_type = EVENT;
     board[22].property_group = NONE;
+    board[22].purchase_price = 0;
+    board[22].base_rent = 0;
 
     board[23].square_name = "Peradeniya";
     board[23].square_type = PROPERTY;
     board[23].property_group = RED;
+    board[23].purchase_price = 5800;
+    board[23].base_rent = 480;
 
     board[24].square_name = "Katugastota";
     board[24].square_type = PROPERTY;
     board[24].property_group = RED;
+    board[24].purchase_price = 6000;
+    board[24].base_rent = 500;
 
     board[25].square_name = "Galle Railway Station";
     board[25].square_type = RAILWAY;
     board[25].property_group = NONE;
+    board[25].purchase_price = 0;
+    board[25].base_rent = 0;
 
     board[26].square_name = "Galle Fort";
     board[26].square_type = PROPERTY;
     board[26].property_group = YELLOW;
+    board[26].purchase_price = 6500;
+    board[26].base_rent = 600;
 
     board[27].square_name = "Unawatuna";
     board[27].square_type = PROPERTY;
     board[27].property_group = YELLOW;
+    board[27].purchase_price = 6800;
+    board[27].base_rent = 620;
 
     board[28].square_name = "National Water Supply and Drainage Board";
     board[28].square_type = UTILITY;
     board[28].property_group = NONE;
+    board[28].purchase_price = 0;
+    board[28].base_rent = 0;
 
     board[29].square_name = "Hikkaduwa";
     board[29].square_type = PROPERTY;
     board[29].property_group = YELLOW;
+    board[29].purchase_price = 7000;
+    board[29].base_rent = 650;
 
     board[30].square_name = "Go To Jail";
     board[30].square_type = JAIL;
     board[30].property_group = NONE;
+    board[30].purchase_price = 0;
+    board[30].base_rent = 0;
 
     board[31].square_name = "Jaffna Town";
     board[31].square_type = PROPERTY;
     board[31].property_group = GREEN;
+    board[31].purchase_price = 8000;
+    board[31].base_rent = 750;
 
     board[32].square_name = "Nallur";
     board[32].square_type = PROPERTY;
     board[32].property_group = GREEN;
+    board[32].purchase_price = 8300;
+    board[32].base_rent = 780;
 
     board[33].square_name = "Ceylinco Insurance";
     board[33].square_type = INSURANCE;
     board[33].property_group = NONE;
+    board[33].purchase_price = 0;
+    board[33].base_rent = 0;
 
     board[34].square_name = "Trincomalee";
     board[34].square_type = PROPERTY;
     board[34].property_group = GREEN;
+    board[34].purchase_price = 8500;
+    board[34].base_rent = 800;
 
     board[35].square_name = "Jaffna Railway Station";
     board[35].square_type = RAILWAY;
     board[35].property_group = NONE;
+    board[35].purchase_price = 0;
+    board[35].base_rent = 0;
 
     board[36].square_name = "National Event Card";
     board[36].square_type = EVENT;
     board[36].property_group = NONE;
+    board[36].purchase_price = 0;
+    board[36].base_rent = 0;
 
     board[37].square_name = "Nuwara Eliya";
     board[37].square_type = PROPERTY;
     board[37].property_group = DARK_BLUE;
+    board[37].purchase_price = 10000;
+    board[37].base_rent = 1000;
 
     board[38].square_name = "Bank of Ceylon";
     board[38].square_type = BANK;
     board[38].property_group = NONE;
+    board[38].purchase_price = 0;
+    board[38].base_rent = 0;
 
     board[39].square_name = "Galle Face";
     board[39].square_type = PROPERTY;
     board[39].property_group = DARK_BLUE;
+    board[39].purchase_price = 12000;
+    board[39].base_rent = 1200;
 
     for (int i = 0; i < MAX_SQUARES; i++)
     {
@@ -194,6 +274,4 @@ void initialize_board(Square *board)
 
         board[i].hotel.condition = 1; // intialized condition of hotel to 100%
     }
-
-    board[9].base_rent = 100;
 }
